c34bc794c215e39e40b65510748cf63a053259a1
Ardin Fardiansyah <ardfarde@gmail.com>
2020-10-24 20:28:06 +0700

    fix test

--- src/test/scala/net/ardfard/QueueLiveTest.scala
+++ src/test/scala/net/ardfard/QueueLiveTest.scala
@@ -1,30 +1,39 @@
-import net.ardfard.mergetrain._
+package net.ardfard.mergetrain
+
 import zio.test._
 import zio.test.Assertion._
 import zio.ZIO
 import zio.stream.ZStream.Pull
 import net.ardfard.mergetrain
 
-object QueueLiveTest {
+object QueueLiveSpec extends DefaultRunnableSpec {
   import queue._
   val prs = Seq(
     PullRequest("1", "test"),
     PullRequest("2", "test2"),
     PullRequest("3", "test3")
   )
-  val live = suite("")(
-    testM("When push PRs in sequence, get back PRs the in the correct order") {
-      val prs = Seq(PullRequest("1", "test"), PullRequest("2", "test2"))
-      val insertPRs = ZIO.foreach(prs)(pr => Queue.push(pr, 0))
-      val getPRs = ZIO.foreach(0 to (prs.length - 1))(pos => Queue.getAt(pos))
 
-      assertM(insertPRs *> getPRs)(equalTo(prs))
-    },
-    testM("After inserted, PR can be removed") {
-      val insertPRs = ZIO.foreach(prs)(pr => Queue.push(pr, 0))
-      val removePR = Queue.remove(prs(1))
-      assertM(insertPRs *> Queue.getAt(1))(equalTo(prs(1))) *>
-        assertM(removePR *> Queue.getAt(1))(equalTo(prs(2)))
-    }
-  )
+  override def spec =
+    suite("Live redis")(
+      testM(
+        "When push PRs in sequence, get back PRs the in the correct order"
+      ) {
+        val insertPRs = ZIO.foreach(prs)(pr => Queue.push(pr, 0))
+        val getPRs = ZIO.foreach(0 to (prs.length - 1))(pos => Queue.getAt(pos))
+        val testEff = (insertPRs *> getPRs).provideLayer(queue.Queue.live)
+
+        assertM(testEff)(equalTo(prs))
+      },
+      testM("After inserted, PR can be removed") {
+        val insertPRs = ZIO.foreach(prs)(pr => Queue.push(pr, 0))
+        val removePR = Queue.remove(prs(1))
+        val testEff1 =
+          (insertPRs *> Queue.getAt(1)).provideLayer(queue.Queue.live)
+        val testEff2 =
+          (removePR *> Queue.getAt(1)) provideLayer (queue.Queue.live)
+        assertM(testEff1)(equalTo(prs(1))) *>
+          assertM(testEff2)(equalTo(prs(2)))
+      }
+    )
 }
